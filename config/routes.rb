ef2238d00ce72a8bc27f309f231bf3e983d70627
Nathaniel Wroblewski <nathanielwroblewski@gmail.com>
2013-08-12 21:04:59 -0700

    Add temporary nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ ProjectBOA::Application.routes.draw do
   mount S3Multipart::Engine => "/s3_multipart"
   root to: 'users#new'
   get '/sessions/destroy' => 'sessions#destroy', as: 'logout'
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
   resources :sessions, only: :create
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :positions, only: :index
